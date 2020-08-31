78e89398882a3fd26da9976ed6796ea6b352c27a
Basel <basel@gmx.co.uk>
2020-08-31 14:34:28 +0100

    Update the styling

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,22 @@ function App() {
   return (
       <main>
         <h3>Selected terms:</h3>
-        <input placeholder={"Terms"}/>
-        <input type={"submit"} value={"Add"} disabled/>
+          <div className={'row'}>
+              <form autoComplete={'off'} className={'col-6'}>
+                  <div className={'autocomplete'}>
+                      <input id={'term_finder'} type={'text'} placeholder='Terms' />
+                  </div>
+                  <input type={'submit'} value={'Add'} disabled/>
+              </form>
+              <div className={'col-6'}>
+                  <h3>Parent terms:</h3>
+                  <div className={'no_term_items col-3'}>None</div>
+                  <h3>Child terms:</h3>
+                  <div className={'no_term_items col-3'}>None</div>
+                  <h3>Related terms:</h3>
+                  <div className={'no_term_items col-3'}>None</div>
+              </div>
+          </div>
       </main>
   );
 }
