7c350cccbb500d419935f91d8a8b4346dda49de4
Basel <basel@gmx.co.uk>
2020-08-31 16:39:56 +0100

    Remove not required files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
+/*global CustomElement*/
 import React from 'react';
+import './App.css';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+CustomElement.init((element, _context) => {
+
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+CustomElement.setHeight(250);
