c551230968d615088502b188fcdaa54629fea42f
Basel <basel@gmx.co.uk>
2020-09-04 17:38:29 +0100

    add prettier and typescript eslint support

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true
+        }
+    },
+    settings: {
+        react: {
+            version: "detect"
+        }
+    },
+    plugins: [
+        '@typescript-eslint',
+    ],
+    extends: [
+        "plugin:react/recommended",
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        "prettier/@typescript-eslint",
+        "plugin:prettier/recommended"
+    ],
+};
