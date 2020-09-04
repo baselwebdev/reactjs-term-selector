51ff5a7ac4a8ceaa97c0e211561d95fd99830de9
Basel <basel@gmx.co.uk>
2020-09-04 17:41:30 +0100

    add prettier and typescript eslint support

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import SearchForm from './components/SearchBox';
+import NoneButton from './components/NoneButton';
+
+function App() {
+    return (
+        <main>
+            <h3>Selected terms:</h3>
+            <div className={'row'}>
+                <SearchForm />
+                <div className={'col-6'}>
+                    <h3>Parent terms:</h3>
+                    <NoneButton />
+                    <h3>Child terms:</h3>
+                    <NoneButton />
+                    <h3>Related terms:</h3>
+                    <NoneButton />
+                </div>
+            </div>
+        </main>
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
-// /*global CustomElement*/
 import React from 'react';
 import './App.css';
 import ReactDOM from 'react-dom';
 import App from './App';
-
-// CustomElement.init((element, _context) => {
-//
-// });
+// import {document} from ''
 
 ReactDOM.render(
     <React.StrictMode>
         <App />
     </React.StrictMode>,
-    document.getElementById('root')
+    document.getElementById('root'),
 );
-
-// CustomElement.setHeight(250);
