4e8e83cfdc381056f28c71615ba4ed0c444f3cd5
Basel <basel@gmx.co.uk>
2020-09-06 15:03:55 +0100

    remove store files

--- src/index.tsx
+++ src/index.tsx
@@ -2,25 +2,10 @@ import React from 'react';
 import './App.css';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Provider } from 'react-redux';
-import configureStore from './store/store';
-import { ApplicationState } from './store';
-
-const initialState = (): ApplicationState => {
-    return {
-        userInput: {
-            searchText: 'Hello',
-        },
-    };
-};
-
-const store = configureStore(initialState());
 
 ReactDOM.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
+        <App />
     </React.StrictMode>,
     document.getElementById('root'),
 );
--- src/store/UserInput/actions.ts
+++
@@ -1,5 +0,0 @@
-import { action } from 'typesafe-actions';
-import { UserInputActionTypes } from './types';
-
-export const getUserInput = () => action(UserInputActionTypes.GET_USER_INPUT);
-export const setUserInput = (searchString: string) => action(UserInputActionTypes.SET_USER_INPUT, searchString);
