4a5a78272a93529c43589cda1693bf9dec0840dd
Basel <basel@gmx.co.uk>
2020-09-16 12:02:28 +0100

    comment out code tht is breaking currently

--- src/features/terms/reducer.ts
+++ src/features/terms/reducer.ts
@@ -2,25 +2,25 @@ import { Term } from 'MyModels';
 import { combineReducers } from 'redux';
 import { createReducer } from 'typesafe-actions';
 
-import { loadTermsAsync, addTerm, removeTerm } from './actions';
+import { loadTermsAsync } from './actions';
 
 export const terms = createReducer([
     {
-        id: '0',
-        name: 'You can add new todos using the form or load saved snapshot...',
+        id: '0001',
+        name: 'My term',
     },
 ] as Term[])
     .handleAction(
         loadTermsAsync.success,
         (state: any, action: any) => action.payload,
-    )
-    .handleAction(addTerm, (state: any, action: any) => [
-        ...state,
-        action.payload,
-    ])
-    .handleAction(removeTerm, (state: any, action: any) =>
-        state.filter((i: any) => i.id !== action.payload),
     );
+    // .handleAction(addTerm, (state: any, action: any) => [
+    //     ...state,
+    //     action.payload,
+    // ])
+    // .handleAction(removeTerm, (state: any, action: any) =>
+    //     state.filter((i: any) => i.id !== action.payload),
+    // );
 
 const termsReducer = combineReducers({
     terms,
