d2ec59e200445bd9dd7287fb4f7edb37f7fd3224
Basel <basel@gmx.co.uk>
2020-09-23 15:33:19 +0100

    refactor test code

--- src/tests/SearchBox.test.tsx
+++ src/tests/SearchBox.test.tsx
@@ -3,12 +3,28 @@ import { render } from './test-utils';
 import SearchBox from '../features/terms/components/SearchBox';
 import userEvent from '@testing-library/user-event';
 
-describe('SearchBox', () => {
-    const { getByPlaceholderText, getByText } = render(<SearchBox />);
-    it('Render found terms', async () => {
-        await userEvent.type(getByPlaceholderText('Terms'), 'Hand');
-        // The Hand search string is surrounded by a <strong> element.
-        expect(getByText('protection')).toBeInTheDocument();
-        expect(getByText('washing')).toBeInTheDocument();
+describe('SearchBox searching showing found term results', () => {
+    let foundTermOne: HTMLElement;
+    let foundTermTwo: HTMLElement;
+    let searchInput: HTMLElement;
+
+    beforeEach(async () => {
+        const { getByPlaceholderText, getByText } = render(<SearchBox />);
+        searchInput = getByPlaceholderText('Terms');
+        await userEvent.type(searchInput, 'Hand');
+        // Hand search string is surrounded by a <strong> element, so we only can search for the non strong element.
+        foundTermOne = getByText('protection');
+        foundTermTwo = getByText('washing');
+    });
+
+    it('Renders found terms', async () => {
+        expect(foundTermOne).toBeInTheDocument();
+        expect(foundTermTwo).toBeInTheDocument();
+    });
+
+    it('Removes the search results when clearing the search input', async () => {
+        await userEvent.type(searchInput, '{selectall}{del}');
+        expect(foundTermOne).not.toBeInTheDocument();
+        expect(foundTermTwo).not.toBeInTheDocument();
     });
 });
