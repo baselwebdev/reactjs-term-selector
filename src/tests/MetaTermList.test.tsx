fb2f6dd54278f16f3bb0852f90761fde5df21ae6
Basel <basel@gmx.co.uk>
2020-09-24 12:13:41 +0100

    add test when searching for a term and then removing character to make it a non found term the meta term section is cleared

--- src/tests/MetaTermList.test.tsx
+++ src/tests/MetaTermList.test.tsx
@@ -7,27 +7,51 @@ import { unmountComponentAtNode } from 'react-dom';
 
 let app: RenderResult;
 let searchInput: HTMLElement;
+const searchTerm = 'Working at height';
 
-beforeEach(() => {
+beforeEach(async () => {
     app = render(<App />);
     searchInput = app.getByPlaceholderText('Terms');
+    await userEvent.type(searchInput, searchTerm);
 });
 
 afterEach(() => {
     unmountComponentAtNode(app.container);
 });
 
-describe('Searching for term, the meta term section', () => {
-    it('Populates the section with the relevant terms for the searched term', async () => {
-        await userEvent.type(searchInput, 'Working at height');
+describe('When searching for terms the meta terms section', () => {
+    it('Populates the parent term section', () => {
         expect(app.getByText('Physical hazards')).toBeInTheDocument();
+    });
+
+    it('Populates the child term section', () => {
         expect(app.getByText('Scaffolding')).toBeInTheDocument();
+    });
+
+    it('Populates the related term section', () => {
         expect(app.getByText('Roofs')).toBeInTheDocument();
     });
 
     it('Populates the search input when clicking on them', async () => {
-        await userEvent.type(searchInput, 'Working at height');
         app.getByText('Physical hazards').click();
         expect(searchInput.closest('input')?.value).toBe('Physical hazards');
+    });
+});
+
+describe('When searching for terms the meta terms section and then removing a character', () => {
+    beforeEach(async () => {
+        await userEvent.type(searchInput, `${searchTerm}{backspace}`);
+    });
+
+    it('Clears the parent term list', () => {
+        expect(app.queryByText('Physical hazards')).not.toBeInTheDocument();
+    });
+
+    it('Clears the child term list', () => {
+        expect(app.queryByText('Scaffolding')).not.toBeInTheDocument();
+    });
+
+    it('Clears the related term list', () => {
+        expect(app.queryByText('Roofs')).not.toBeInTheDocument();
     });
 });
